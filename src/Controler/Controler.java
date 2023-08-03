82ceef9bed886040ee5c0ca8cd6994867e8d900a
EkarCortes <Ekardaco@gmail.com>
2023-08-02 21:04:35 -0600

    Creacion del paquete Controler
    Se crea  el paquete Controler, y ademas de una interfaz generica con sus parametros type

---
+++ src/Controler/Controler.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Controler;
+
+/**
+ *
+ * @author ekard
+ */
+public interface Controler<Type> {
+ public void insert(Type obj);
+ public void update(Type obj);
+ public void delete(Type obj);
+ public void read(Object id);
+ public void readAll();
+}
+
