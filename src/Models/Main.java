d033c3d7962288bc2ef790b2af464f703aabc5e0
abiga <abigailmolina201@gmail.com>
2023-08-10 15:42:41 -0600

    Main
    Se crea la clase Main

---
+++ src/Models/Main.java
@@ -0,0 +1,15 @@
+package Models;
+
+/**
+ *
+ * @author abiga
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Alejandra");
+ }
+}
